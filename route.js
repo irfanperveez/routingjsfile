34a25ad456c1e746ef4490d93d36dabd8398e9f5
irfanperveez <ipervez@leeddev.io>
2023-06-08 04:24:43 -0700

    commit

--- route.js
+++ route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-app.use(express.json());
+app.use(express.json());//kkj
 app.post('/api/data', (req, res) => {
   const data = req.body;
   // Convert the data object to a string
